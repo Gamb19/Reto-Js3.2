bf863fd696f5e54ca79f708c352098ee276a4b67
Gamb19 <molinaborges02@gmail.com>
2022-09-26 10:40:20 -0500

    last commit

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 
 let hotel;
 let dataHotel
-
+let validacion=false;
 let hotelPromise = hotel =fetch("https://my-json-server.typicode.com/manuelmebm/testing-hotel-api/hotels")
 .then((response)=> response.json())
 .then((data)=>{
@@ -13,7 +13,7 @@ getHotels();
 })
 
 function getHotels() {
-
+//creacion html
         dataHotel.forEach(element => {
             let section= document.getElementById("hoteles")
 
@@ -38,14 +38,28 @@ function getHotels() {
       
             let textDes = document.createElement("p");
             textDes.textContent = element.description;
+
+            let buttonBack= document.getElementById("button-back");
+            buttonBack.className="button back displays"
+            console.log(buttonBack);
             
             let like = document.createElement("i");
             like.className="fa-regular fa-heart"
             like.addEventListener("click", (e)=>{
-
-              likeFunction(e,element.id);
-
+              likeFunction(like,element.id);
             })
+            if (element.like=="likeado") {
+              like.className="fa-regular fa-heart red"
+            }else{
+              like.className="fa-regular fa-heart"
+            }
+
+            let buttonLiked=document.getElementById("button");
+          
+            buttonLiked.addEventListener("click",buttonfilter)
+            
+            
+            
             
       // Html
           section.insertAdjacentElement("beforeend",div_Hotel);
@@ -55,7 +69,8 @@ function getHotels() {
           div_Hotel.insertAdjacentElement("beforeend",descrip);
           descrip.insertAdjacentElement("beforeend",titles);
           descrip.insertAdjacentElement("beforeend",textDes);
-      
+
+            //logica estrellas
           let grayStar = 5 - element.rating;
             for(i=0; i <  element.rating; i++){
               let star = document.createElement("i");
@@ -69,7 +84,98 @@ function getHotels() {
             }
           });
     }
+//Corazones
+function likeFunction(like,id){
 
-function likeFunction(e){
-console.log("hola");
+dataHotel.forEach(element=>{
+  if(element.id===id){
+    if(element.like==="likeado"){
+    element.like="deslike"
+    like.className="fa-regular fa-heart";
+    if(validacion=true){
+      buttonfilter();
+    }
+    }
+  else{
+    element.like="likeado"
+    like.className="fa-regular fa-heart red"
+  }
+}
+})
+}
+
+function buttonfilter(){
+  validacion=true
+  let section= document.getElementById("hoteles")
+      section.innerHTML="";
+  dataHotel.forEach(element=>{
+    if(element.like=="likeado"){
+      
+            let div_Hotel = document.createElement("div");
+            div_Hotel.className="div-hotel"
+            let div_img = document.createElement("div");
+            div_img.className="div_img"
+      
+            let imagen = document.createElement("img")
+            imagen.src=element.thumbnail;
+            imagen.className='img'
+
+            let descrip = document.createElement("div");
+            descrip.className="div-container"
+      
+            let titles = document.createElement("a");
+            titles.href=`hoteles.html?id=${element.id}`
+            titles.textContent=element.title;
+            titles.addEventListener("click", (e)=> {
+               titleHotel(element.id, element.rating, element.thumbnail,element.description, element.title)
+              })
+      
+            let textDes = document.createElement("p");
+            textDes.textContent = element.description;
+
+            let buttonBack= document.getElementById("button-back");
+            buttonBack.className="button back displaysBlock"
+            console.log(buttonBack);
+            
+            
+            let like = document.createElement("i");
+            like.className="fa-regular fa-heart"
+            like.addEventListener("click", (e)=>{
+              likeFunction(like,element.id);
+            })
+            if (element.like=="likeado") {
+              like.className="fa-regular fa-heart red"
+            }else{
+              like.className="fa-regular fa-heart"
+            }
+            
+            let buttonLiked=document.getElementById("button");
+            buttonLiked.addEventListener("click",buttonfilter);
+            buttonLiked.className="displays";
+
+            
+      // Html
+          section.insertAdjacentElement("beforeend",div_Hotel);
+          div_Hotel.insertAdjacentElement("beforeend",div_img);
+          div_img.insertAdjacentElement("beforeend",imagen);
+          div_img.insertAdjacentElement("beforeend", like);
+          div_Hotel.insertAdjacentElement("beforeend",descrip);
+          descrip.insertAdjacentElement("beforeend",titles);
+          descrip.insertAdjacentElement("beforeend",textDes);
+          
+
+            //logica estrellas
+          let grayStar = 5 - element.rating;
+            for(i=0; i <  element.rating; i++){
+              let star = document.createElement("i");
+              star.className="fa-solid fa-star";
+              descrip.insertAdjacentElement("beforeend",star);
+            }
+            for(j=0; j<grayStar; j++){
+              let star = document.createElement("i");
+              star.className="fa-regular fa-star gray-star";
+              descrip.insertAdjacentElement("beforeend",star);
+            }
+    }
+  })
 }
